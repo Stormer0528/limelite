5dc1cd1a94e55cb902895996d1f92bb429bfa496
Vladimir Prelic <vladimirprelic22@gmail.com>
2024-05-24 17:06:02 -0400

    Fix AP tile permission for super-admin and 4 people

--- app/policies/file_policy.rb
+++ app/policies/file_policy.rb
@@ -4,6 +4,12 @@ class FilePolicy < ApplicationPolicy
   end
 
   def view?
+    return true if escalated_priveledges?
+
+    if @user_group.nil? && ["apanalyst01@iconsm.com", "avickers@iconsm.com", "eson@iconsm.com", "jneddersen@iconsm.com"].include?(@current_user.email)
+      return true
+    end
+
     @current_user.ap? || ["Owner", "Editor", "Viewer"].include?(@user_group&.module_permissions&.dig("Report"))
   end
 end
