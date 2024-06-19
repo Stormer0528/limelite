6d98c11c7226396ddc2d37868d1c5164426b5088
vitalii <vitovodenko@gmail.com>
2024-06-19 10:31:28 -0400

    Clean up nav bar

--- src/layouts/config-nav-dashboard.tsx
+++ src/layouts/config-nav-dashboard.tsx
@@ -2,7 +2,7 @@ import { paths } from 'src/routes/paths';
 
 import { CONFIG } from 'src/config';
 
-import { SvgColor } from 'src/components/svg-color';
+import { SvgColor } from 'src/components/SvgColor';
 
 // ----------------------------------------------------------------------
 
@@ -11,32 +11,8 @@ const icon = (name: string) => (
 );
 
 const ICONS = {
-  job: icon('ic-job'),
-  blog: icon('ic-blog'),
-  chat: icon('ic-chat'),
-  mail: icon('ic-mail'),
-  user: icon('ic-user'),
-  file: icon('ic-file'),
-  lock: icon('ic-lock'),
-  tour: icon('ic-tour'),
-  order: icon('ic-order'),
-  label: icon('ic-label'),
-  blank: icon('ic-blank'),
-  kanban: icon('ic-kanban'),
-  folder: icon('ic-folder'),
-  course: icon('ic-course'),
-  banking: icon('ic-banking'),
-  booking: icon('ic-booking'),
-  invoice: icon('ic-invoice'),
-  product: icon('ic-product'),
-  calendar: icon('ic-calendar'),
-  disabled: icon('ic-disabled'),
-  external: icon('ic-external'),
-  menuItem: icon('ic-menu-item'),
-  ecommerce: icon('ic-ecommerce'),
-  analytics: icon('ic-analytics'),
-  dashboard: icon('ic-dashboard'),
-  parameter: icon('ic-parameter'),
+  user: icon('ic_user'),
+  school: icon('ic_school'),
 };
 
 // ----------------------------------------------------------------------
@@ -46,29 +22,10 @@ export const navData = [
    * Overview
    */
   {
-    subheader: 'Overview 6.0.0',
+    subheader: 'Limelite Administration',
     items: [
-      { title: 'One', path: paths.dashboard.root, icon: ICONS.dashboard },
-      { title: 'Two', path: paths.dashboard.two, icon: ICONS.ecommerce },
-      { title: 'Three', path: paths.dashboard.three, icon: ICONS.analytics },
-    ],
-  },
-  /**
-   * Management
-   */
-  {
-    subheader: 'Management',
-    items: [
-      {
-        title: 'Group',
-        path: paths.dashboard.group.root,
-        icon: ICONS.user,
-        children: [
-          { title: 'Four', path: paths.dashboard.group.root },
-          { title: 'Five', path: paths.dashboard.group.five },
-          { title: 'Six', path: paths.dashboard.group.six },
-        ],
-      },
+      { title: 'User', path: paths.dashboard.user.root, icon: ICONS.user },
+      { title: 'Organization', path: paths.dashboard.org.root, icon: ICONS.school },
     ],
   },
 ];
