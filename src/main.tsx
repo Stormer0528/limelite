828b77f4c0b05931ef515680afdd2a545cb0eb0f
vitalii <vitovodenko@gmail.com>
2024-06-20 11:28:46 -0400

    Remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
+import { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { Suspense, StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
@@ -11,13 +11,11 @@ import { CONFIG } from './config';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter basename={CONFIG.site.basePath}>
-        <Suspense>
-          <App />
-        </Suspense>
-      </BrowserRouter>
-    </HelmetProvider>
-  </StrictMode>
+  <HelmetProvider>
+    <BrowserRouter basename={CONFIG.site.basePath}>
+      <Suspense>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  </HelmetProvider>
 );
