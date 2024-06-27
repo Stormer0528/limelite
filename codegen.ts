827869518791df763cb4af11fcb735ee3eea7387
vitalii <vitovodenko@gmail.com>
2024-06-27 05:02:24 -0400

    Fix codegen config for graphql server url change

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:4000',
+  schema: 'http://localhost:4000/graphql',
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   documents: ['src/**/*.{ts,tsx}'],
   generates: {
