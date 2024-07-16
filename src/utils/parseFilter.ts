d908e9d68580ed1dc77798007b7f9ecb104e6f47
vitalii <vitovodenko@gmail.com>
2024-07-16 05:46:22 -0400

    Fix parsing undefined filter value

--- src/utils/parseFilter.ts
+++ src/utils/parseFilter.ts
@@ -5,7 +5,11 @@ export function parseFilter(curFilter: any = {}, filter?: GridFilterModel) {
 
   (filter?.items ?? []).forEach((item) => {
     const { value, field, operator } = item;
-    if (value !== undefined) {
+    if (operator === 'isEmpty') {
+      filterObj[field] = null;
+    } else if (operator === 'isNotEmpty') {
+      filterObj[field] = { not: null };
+    } else if (value !== undefined) {
       if (operator === '=' || operator === 'equals') {
         filterObj[field] = value;
       } else if (operator === 'is') {
@@ -23,15 +27,11 @@ export function parseFilter(curFilter: any = {}, filter?: GridFilterModel) {
       } else if (operator === 'isAnyOf') {
         filterObj[field] = { in: value };
       } else if (operator === 'contains') {
-        filterObj[field] = { contains: `%${null}%`, mode: 'insensitive' };
+        filterObj[field] = { contains: `%${value}%`, mode: 'insensitive' };
       } else if (operator === 'startsWith') {
-        filterObj[field] = { startsWith: null, mode: 'insensitive' };
+        filterObj[field] = { startsWith: value, mode: 'insensitive' };
       } else if (operator === 'endsWith') {
-        filterObj[field] = { endsWith: null, mode: 'insensitive' };
-      } else if (operator === 'isEmpty') {
-        filterObj[field] = null;
-      } else if (operator === 'isNotEmpty') {
-        filterObj[field] = { not: null };
+        filterObj[field] = { endsWith: value, mode: 'insensitive' };
       }
     }
   });
